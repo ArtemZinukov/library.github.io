bd1ccb2ebc9d3579d8540b4d207faae046027488
Artem <zinykov_a@mail.ru>
2024-08-01 13:08:42 +0300

    Изменил загрузку файла json через load

--- render_website.py
+++ render_website.py
@@ -1,4 +1,5 @@
 import json
+import argparse
 import os
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
@@ -17,10 +18,10 @@ def create_pages_directory(directory):
         os.makedirs(directory)
 
 
-def load_books():
-    with open("media/books_info.json", "r", encoding='utf-8') as file:
-        books_json = file.read()
-    books = json.loads(books_json)
+def load_books(dest_folder):
+    file_path = os.path.join(dest_folder, "books_info.json")
+    with open(file_path, "r", encoding='utf-8') as file:
+        books = json.load(file)
     return books
 
 
@@ -36,8 +37,8 @@ def render_page(page_books, page_num, total_pages):
         file.write(rendered_page)
 
 
-def on_reload():
-    books = load_books()
+def on_reload(dest_folder):
+    books = load_books(dest_folder)
     books_per_page = 20
     chunked_books = list(chunked(books, books_per_page))
     total_pages = len(chunked_books)
@@ -46,14 +47,26 @@ def on_reload():
         render_page(page_books, page_num, total_pages)
 
 
+def create_parser():
+    parser = argparse.ArgumentParser(prog='render_website',
+                                     description='запускает скрипт для создания сайта с книгами')
+    parser.add_argument('--dest_folder', help="Укажите путь, где хранится файл json",
+                        type=str, default='media/')
+    return parser
+
+
 def main():
+    parser = create_parser()
+    parser_args = parser.parse_args()
+
     create_pages_directory(pages_dir)
-    on_reload()
+
+    on_reload(parser_args.dest_folder)
 
     server = Server()
-    server.watch('media/books/books_info.json', on_reload)
+    server.watch('media/books_info.json', on_reload)
     server.watch('media/books/*.txt', on_reload)
-    server.watch('media/books/*.jpg', on_reload)
+    server.watch('media/images/*.jpg', on_reload)
     server.watch('template.html', on_reload)
     server.watch('pages/*.html', on_reload)
 
